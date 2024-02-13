eeab297c2c99be8eef7b6a01955a971e9eb58daf
Azzam <48726420+omxqn@users.noreply.github.com>
2024-02-13 20:31:22 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+# pip install torch==2.0.1+cu118 torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+# pip install ultralytics
+import torch
+import torchvision
+
+from ultralytics import YOLO
+
+model = YOLO("yolov8m.pt")
+result = model.train(data="data.yaml", epochs=100, imgz= 640, device=0,workers=0)
+
